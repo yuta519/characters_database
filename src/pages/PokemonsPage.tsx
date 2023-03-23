004d4a6dcdf8696c85be6db2b78999badea73bd1
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-03-23 13:08:33 +0900

    Add card action

--- src/pages/PokemonsPage.tsx
+++ src/pages/PokemonsPage.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
-import styled from "@emotion/styled";
 import { Box } from "@mui/material";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Grid from "@mui/material/Unstable_Grid2";
+import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 
 import { FetchPokemons } from "../api/PokemonApi";
@@ -29,7 +29,6 @@ const Pokemons = () => {
 
   return (
     <>
-      <CssBaseline />
       <h1>POKéMON</h1>
       <h2>{state.count}</h2>
       <Box sx={{ flexGrow: 1 }}>
@@ -38,18 +37,26 @@ const Pokemons = () => {
             return (
               <Grid key={pokemonInfo.id} xs={3}>
                 <Card sx={{ minWidth: 275 }}>
-                  <CardContent>
-                    <CardMedia
-                      sx={{ height: 200 }}
-                      image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonInfo.id}.png`}
-                      title={pokemonInfo.name}
-                    />
-                    <Typography variant="h5" component="div">
-                      {pokemonInfo.name}
-                    </Typography>
-                  </CardContent>
+                  <CardActionArea>
+                    <Link
+                      href={pokemonInfo.id}
+                      color="inherit"
+                      underline="none"
+                    >
+                      <CardContent>
+                        <CardMedia
+                          sx={{ height: 200 }}
+                          image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonInfo.id}.png`}
+                          title={pokemonInfo.name}
+                        />
+                        <Typography variant="h5" component="div">
+                          {pokemonInfo.name}
+                        </Typography>
+                      </CardContent>
+                    </Link>
+                  </CardActionArea>
                   <CardActions>
-                    <Button size="small">Learn More</Button>
+                    <Button size="small">SHARE</Button>
                   </CardActions>
                 </Card>
               </Grid>

d430cd1601c22843a030273997f3767114fea77d
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-03-24 13:24:51 +0900

    erase unnecessary tags and importing

--- src/pages/WorksPage.tsx
+++ src/pages/WorksPage.tsx
@@ -1,31 +1,15 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import {
   Box,
   Card,
   CardActionArea,
-  CardActions,
   CardContent,
   CardMedia,
   Typography,
 } from "@mui/material";
-import Button from "@mui/material/Button";
-import styled from "@emotion/styled";
 import Grid from "@mui/material/Unstable_Grid2";
 import Link from "@mui/material/Link";
 
-import { FetchPokemons } from "../api/PokemonApi";
-
 const Works = () => {
-  const [state, update] = useState({});
-  const params = useParams();
-
-  useEffect(() => {
-    (async () => {
-      update(await FetchPokemons());
-    })();
-  }, []);
-
   return (
     <>
       <h1>Quizes</h1>
@@ -58,14 +42,10 @@ const Works = () => {
           <Grid xs={3}>xs=8</Grid>
         </Grid>
       </Box>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      {/* <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button> */}
     </>
   );
 };
-
-const StyledBox = styled(Box)`
-  margin: 100px;
-`;
 
 export default Works;

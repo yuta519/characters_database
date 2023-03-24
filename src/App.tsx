ca7024e5b1a0d21dc69756a3a382ec1fff2bca2c
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-03-24 12:38:23 +0900

    Add title and remove imporiting of css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import React from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
+// import "./App.css";
 import { Button } from "@mui/material";
 
 function App() {
